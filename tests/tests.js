821988bb88554815c5340219b07be5c3be15fff0
Richard Silverton <silverton.richard@googlemail.com>
2016-02-12 17:43:32 +0000

    Wrote basic tests

--- tests/tests.js
+++ tests/tests.js
@@ -1,12 +1,27 @@
 var test = require('tape')
+var neural = require('../neural')
 
-test('timing test', function(t) {
-  t.plan(2)
+test('creation of atomic units', function(t) {
+  t.plan(3)
 
-  t.equal(typeof Date.now, 'function')
-  var start = Date.now()
+  var neuron = new neural.Neuron({
+    inputs: [
+      function () { return new Date().getTime() },
+      1
+    ],
+    weights: [
+      0.5,
+      0.5
+    ]
+  })
 
-  setTimeout(function() {
-    t.equal(Date.now() - start, 100)
-  }, 100)
+  t.ok(neuron instanceof neural.Neuron, 'Can create a Neuron')
+
+  var layer = new neural.Layer({ neurons: 3 })
+  t.ok(layer instanceof neural.Layer, 'Can create a Layer')
+
+  var network = new neural.Network({
+    layers: [3, 4, 2]
+  })
+  t.ok(network instanceof neural.Network, 'Can create a Network')
 })
